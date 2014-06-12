7d32fc9277a5094d5fd3e4289aac91a1f3a7104d
army8735 <army8735@qq.com>
2014-06-12 13:07:32 +0800

    update

--- jsdc/web/Generator.js
+++ jsdc/web/Generator.js
@@ -7,13 +7,12 @@ define(function(require, exports, module) {
   var Generator = Class(function(jsdc) {
     this.jsdc = jsdc;
     this.hash = {};
-    this.yie = {};
+    this.star = {};
   }).methods({
     parse: function(node, start) {
       if(start) {
         this.jsdc.ignore(node.leaf(1));
         var token = node.leaf(2).first().token();
-        var id = token.content();
         //有可能被scope前置过
         var hasPre = token.ignore;
         //忽略本身
@@ -29,17 +28,44 @@ define(function(require, exports, module) {
         var o = this.hash[node.nid()] = {
           state: state,
           index: 0,
-          temp: temp
+          temp: temp,
+          yield: []
         };
         this.jsdc.append('function(){');
         this.jsdc.append('var ' + state + '=0;');
         this.jsdc.append('return ');
-        this.jsdc.append('function (){return {next:' + temp + '}};');
+        this.jsdc.append('function(){return{next:' + temp + '}};');
         o.pos = this.jsdc.res.length;
         this.jsdc.append('function ' + temp);
       }
       else {
         this.jsdc.appendBefore('}();');
+      }
+    },
+    expr: function(node, start) {
+      if(start) {
+        this.jsdc.ignore(node.first());
+        this.jsdc.ignore(node.leaf(1));
+        if(node.leaf(2).name() == JsNode.BINDID) {
+          this.jsdc.ignore(node.leaf(2));
+        }
+        var state = this.jsdc.uid();
+        var temp = this.jsdc.uid();
+        var o = this.hash[node.nid()] = {
+          state: state,
+          index: 0,
+          temp: temp,
+          yield: []
+        };
+        this.jsdc.append('function(){');
+        this.jsdc.append('var ' + state + '=0;');
+        this.jsdc.append('return ');
+        this.jsdc.append('function(){return{next:' + temp + '}};');
+        o.pos = this.jsdc.res.length;
+        this.jsdc.append('function ' + temp);
+      }
+      else {
+        this.jsdc.appendBefore('}()');
       }
     },
     yield: function(node, start) {
@@ -50,35 +76,65 @@ define(function(require, exports, module) {
           this.jsdc.append('case ' + (o.index - 1) + ':');
         }
         this.jsdc.ignore(node.first());
-        this.jsdc.append('return ');
+        this.jsdc.append('arguments[0];');
         //yield *
         if(node.size() > 2
           && node.leaf(1).name() == JsNode.TOKEN
           && node.leaf(1).token().content() == '*') {
           this.jsdc.ignore(node.leaf(1));
-          this.yie[node.nid()] = true;
+          var temp = this.star[node.nid()] = this.jsdc.uid();
+          this.jsdc.append('var ' + temp + '=');
+        }
+        else {
+          this.jsdc.append('return{value:');
         }
       }
-      else if(this.yie.hasOwnProperty(node.nid())) {
-        this.jsdc.appendBefore('()');
+      else {
+        if(this.star.hasOwnProperty(node.nid())) {
+          var temp = this.star[node.nid()];
+          this.jsdc.appendBefore('();if(!' + temp + '.done)' + o.state + '--;return ' + temp);
+          o.yield.push({
+            i: this.jsdc.i,
+            star: temp
+          });
+        }
+        else {
+          this.jsdc.appendBefore(',done:false}');
+          o.yield.push({
+            i: this.jsdc.i
+          });
+        }
       }
     },
     body: function(node, start) {
       var top = node.parent();
       if(top.name() == JsNode.GENDECL) {
+        var o = this.hash[top.nid()];
         if(start) {
-          var o = this.hash[top.nid()];
           this.jsdc.append('switch(' + o.state + '++){case 0:');
         }
         else {
-          this.jsdc.appendBefore('}');
+          if(o.index) {
+            var yie = o.yield[o.yield.length - 1];
+            var i;
+            if(yie.star) {
+              i = this.jsdc.res.lastIndexOf(yie.star, i);
+              this.jsdc.replace(';default:', i + yie.star.length, 0);
+            }
+            else {
+              i = this.jsdc.res.lastIndexOf(',done:false}', i);
+              this.jsdc.replace('true};default:', i + 6, 6);
+            }
+          }
+          this.jsdc.appendBefore(';return{done:true}}');
         }
       }
     },
     closest: function(node) {
       var parent = node;
       while(parent = parent.parent()) {
-        if(parent.name() == JsNode.GENDECL) {
+        if(parent.name() == JsNode.GENDECL
+          || parent.name() == JsNode.GENEXPR) {
           return parent;
         }
       }
