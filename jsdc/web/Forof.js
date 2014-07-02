3a5929a16c0ff8304d62341ea3162d7c94f2f570
army8735 <army8735@qq.com>
2014-07-02 15:37:11 +0800

    update

--- jsdc/web/Forof.js
+++ jsdc/web/Forof.js
@@ -9,20 +9,47 @@ define(function(require, exports, module) {
   var Forof = Class(function(jsdc) {
     this.jsdc = jsdc;
     this.hash = {};
+    this.pos = {};
+    this.destruct = {};
   }).methods({
     parse: function(node, start) {
-      //有可能被Generator中该写过
+      //有可能被Generator中改写过
       if(node.gen) {
         return;
       }
       if(start) {
-        var of = node.leaf(3);
-        if(node.first().token().content() == 'for'
-          && of.name() == JsNode.TOKEN
-          && of.token().content() == 'of') {
-          //存放临时li供block首尾改写引用
-          this.hash[node.nid()] = true;
-          this.jsdc.ignore(of, 'forof1');
+        if(node.first().token().content() == 'for') {
+          var of = node.leaf(3);
+          if(of.name() == JsNode.TOKEN
+            && of.token().content() == 'of') {
+            this.pos[node.nid()] = 3;
+            //存放临时id供block首改写引用
+            var leaf = node.leaf(2);
+            if(leaf.name() == JsNode.PRMREXPR
+              && leaf.size() == 1
+              && [JsNode.OBJLTR, JsNode.ARRLTR].indexOf(leaf.first().name()) > -1) {
+              this.destruct[node.nid()] = leaf.first();
+              this.hash[node.nid()] = this.getLast(leaf.first());
+              this.jsdc.ignore(leaf, 'forof1');
+            }
+            else {
+              this.hash[node.nid()] = true;
+            }
+            this.jsdc.ignore(of, 'forof2');
+          }
+          //for(var forbind of中为4
+          else {
+            of = node.leaf(4);
+            if(of.name() == JsNode.TOKEN
+              && of.token().content() == 'of') {
+              this.pos[node.nid()] = 4;
+              this.destruct[node.nid()] = node.leaf(3);
+              //存放临时id供block首改写引用
+              this.hash[node.nid()] = this.getLast(node.leaf(3));
+              this.jsdc.ignore(of, 'forof3');
+              this.jsdc.ignore(node.leaf(3), 'forof4');
+            }
+          }
         }
       }
       else if(this.hash.hasOwnProperty(node.nid())) {
@@ -37,6 +64,9 @@ define(function(require, exports, module) {
       var parent = node.parent();
       if(parent.name() == JsNode.ITERSTMT
         && this.hash.hasOwnProperty(parent.nid())) {
+        if(typeof this.hash[parent.nid()] == 'string') {
+          this.jsdc.append(this.hash[parent.nid()]);
+        }
         this.jsdc.append('=');
       }
     },
@@ -46,15 +76,21 @@ define(function(require, exports, module) {
         && this.hash.hasOwnProperty(parent.nid())) {
         if(start) {
           this.jsdc.append('.next();!');
-          var k = parent.leaf(2);
-          //forof的varstmt只能有一个id，其它为mmbexpr
-          var v = join(parent.leaf(4));
-          if(k.name() == JsNode.VARSTMT) {
-            k = k.last().first().first().token().content();
+          var k;
+          if(typeof this.hash[parent.nid()] == 'string') {
+            k = this.hash[parent.nid()];
           }
           else {
-            k = join(k);
+            k = parent.leaf(2);
+            //forof的varstmt只能有一个id，其它为mmbexpr或destruct
+            if(k.name() == JsNode.VARSTMT) {
+              k = k.last().first().first().token().content();
+            }
+            else {
+              k = join(k);
+            }
           }
+          var v = join(parent.leaf(this.pos[parent.nid()]+1));
           this.jsdc.append(k + '.done;');
           this.jsdc.append(k + '=' + v + '.next()');
         }
@@ -81,16 +117,133 @@ define(function(require, exports, module) {
         }
       }
     },
-    assign: function(node) {
-      var k = node.leaf(2);
-      //forof的varstmt只能有一个id，其它为mmbexpr
-      if(k.name() == JsNode.VARSTMT) {
-        k = k.last().first().first().token().content();
+    assign: function(node, ret) {
+      var k;
+      if(ret) {
+        k = ret.o;
+      }
+      else if(typeof this.hash[node.nid()] == 'string') {
+        k = this.hash[node.nid()];
       }
       else {
-        k = join(k);
+        k = node.leaf(2);
+        //forof的varstmt只能有一个id，其它为mmbexpr或destruct
+        if(k.name() == JsNode.VARSTMT) {
+          k = k.last().first().first().token().content();
+        }
+        else {
+          k = join(k);
+        }
       }
-      this.jsdc.append(k + '=' + k + '.value;');
+      if(ret) {
+        if(ret.pos == 4) {
+          this.jsdc.destruct.parse(node, true, ret);
+          this.jsdc.destruct.parse(node, false, ret);
+        }
+        else {
+          this.jsdc.destruct.expr(node, true, ret);
+          this.jsdc.destruct.expr(node, false, ret);
+        }
+        return ret.s;
+      }
+      else {
+        this.jsdc.append(k + '=' + k + '.value;');
+        if(this.destruct.hasOwnProperty(node.nid())) {
+          var ret2 = {
+            o: k,
+            s: '',
+            append: function(s) {
+              this.s += s;
+            },
+            appendBefore: function(s) {
+              this.s += s;
+            }
+          };
+          if(this.pos[node.nid()] == 4) {
+            this.jsdc.destruct.parse(this.destruct[node.nid()], true, ret2);
+            this.jsdc.destruct.parse(this.destruct[node.nid()], false, ret2);
+          }
+          else {
+            this.jsdc.destruct.expr(this.destruct[node.nid()], true, ret2);
+            this.jsdc.destruct.expr(this.destruct[node.nid()], false, ret2);
+          }
+          this.jsdc.append(ret2.s);
+        }
+      }
+    },
+    getLast: function(node) {
+      if(node.name() == JsNode.ARRLTR
+        || node.name() == JsNode.ARRBINDPAT) {
+        return this.getArrLast(node);
+      }
+      else {
+        return this.getObjLast(node);
+      }
+    },
+    getArrLast: function(node) {
+      for(var leaves = node.leaves(), i = leaves.length - 2; i > 0; i--) {
+        var temp = leaves[i];
+        var s = temp.name();
+        if(s == JsNode.SINGLENAME
+          || s == JsNode.ASSIGNEXPR) {
+          return temp.first().first().token().content();
+        }
+        else if(s == JsNode.PRMREXPR) {
+          temp = temp.first();
+          s = temp.name();
+          if(s == JsNode.TOKEN) {
+            return temp.token().content();
+          }
+          else {
+            return this.getLast(temp);
+          }
+        }
+        else if(s == JsNode.BINDELEM) {
+          return this.getLast(temp.first());
+        }
+      }
+    },
+    getObjLast: function(node) {
+      for(var leaves = node.leaves(), i = leaves.length - 2; i > 0; i--) {
+        var temp = leaves[i];
+        var s = temp.name();
+        if(s == JsNode.BINDPROPT) {
+          leaves = temp.leaves();
+          if(leaves.length < 3) {
+            s = leaves[0].name();
+            return leaves[0].first().first().token().content();
+          }
+          else {
+            temp = leaves[2];
+            s = temp.name();
+            if(s == JsNode.SINGLENAME) {
+              return temp.first().first().token().content();
+            }
+            else if(s == JsNode.BINDELEM) {
+              return this.getLast(temp.first());
+            }
+          }
+        }
+        else if(s == JsNode.PROPTDEF) {
+          leaves = temp.leaves();
+          if(leaves.length < 3) {
+            return leaves[0].token().content();
+          }
+          else {
+            temp = leaves[2].first();
+            s = temp.name();
+            if(s == JsNode.TOKEN) {
+              return temp.token().content();
+            }
+            else if(s == JsNode.PRMREXPR) {
+              return temp.first().token().content();
+            }
+            else {
+              return this.getLast(temp);
+            }
+          }
+        }
+      }
     }
   });
   
