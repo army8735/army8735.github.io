2f8e959c3f21f487a65a7b85000de4a30a7b34d9
army8735 <army8735@qq.com>
2015-04-05 09:53:31 +0800

    update

--- more/web/Tree.js
+++ more/web/Tree.js
@@ -1,21 +1,37 @@
-define(function(require, exports, module){var homunculus=require('homunculus');
+define(function(require, exports, module){var homunculus=function(){var _0=require('homunculus');return _0.hasOwnProperty("homunculus")?_0.homunculus:_0.hasOwnProperty("default")?_0.default:_0}();
 
-var join=function(){var _0=require('./join');return _0.hasOwnProperty("join")?_0.join:_0.hasOwnProperty("default")?_0.default:_0}();
-var ignore=function(){var _1=require('./ignore');return _1.hasOwnProperty("ignore")?_1.ignore:_1.hasOwnProperty("default")?_1.default:_1}();
-var getVar=function(){var _2=require('./getVar');return _2.hasOwnProperty("getVar")?_2.getVar:_2.hasOwnProperty("default")?_2.default:_2}();
-var getFn=function(){var _3=require('./getFn');return _3.hasOwnProperty("getFn")?_3.getFn:_3.hasOwnProperty("default")?_3.default:_3}();
-var checkLevel=function(){var _4=require('./checkLevel');return _4.hasOwnProperty("checkLevel")?_4.checkLevel:_4.hasOwnProperty("default")?_4.default:_4}();
-var concatSelector=function(){var _5=require('./concatSelector');return _5.hasOwnProperty("concatSelector")?_5.concatSelector:_5.hasOwnProperty("default")?_5.default:_5}();
-var normalize=function(){var _6=require('./normalize');return _6.hasOwnProperty("normalize")?_6.normalize:_6.hasOwnProperty("default")?_6.default:_6}();
-var operate=function(){var _7=require('./operate');return _7.hasOwnProperty("operate")?_7.operate:_7.hasOwnProperty("default")?_7.default:_7}();
-var ifstmt=function(){var _8=require('./ifstmt');return _8.hasOwnProperty("ifstmt")?_8.ifstmt:_8.hasOwnProperty("default")?_8.default:_8}();
-var forstmt=function(){var _9=require('./forstmt');return _9.hasOwnProperty("forstmt")?_9.forstmt:_9.hasOwnProperty("default")?_9.default:_9}();
-var eventbus=function(){var _10=require('./eventbus');return _10.hasOwnProperty("eventbus")?_10.eventbus:_10.hasOwnProperty("default")?_10.default:_10}();
-var preVar=function(){var _11=require('./preVar');return _11.hasOwnProperty("preVar")?_11.preVar:_11.hasOwnProperty("default")?_11.default:_11}();
-var exprstmt=function(){var _12=require('./exprstmt');return _12.hasOwnProperty("exprstmt")?_12.exprstmt:_12.hasOwnProperty("default")?_12.default:_12}();
+var join=function(){var _1=require('./join');return _1.hasOwnProperty("join")?_1.join:_1.hasOwnProperty("default")?_1.default:_1}();
+var ignore=function(){var _2=require('./ignore');return _2.hasOwnProperty("ignore")?_2.ignore:_2.hasOwnProperty("default")?_2.default:_2}();
+var getVar=function(){var _3=require('./getVar');return _3.hasOwnProperty("getVar")?_3.getVar:_3.hasOwnProperty("default")?_3.default:_3}();
+var getFn=function(){var _4=require('./getFn');return _4.hasOwnProperty("getFn")?_4.getFn:_4.hasOwnProperty("default")?_4.default:_4}();
+var checkLevel=function(){var _5=require('./checkLevel');return _5.hasOwnProperty("checkLevel")?_5.checkLevel:_5.hasOwnProperty("default")?_5.default:_5}();
+var concatSelector=function(){var _6=require('./concatSelector');return _6.hasOwnProperty("concatSelector")?_6.concatSelector:_6.hasOwnProperty("default")?_6.default:_6}();
+var normalize=function(){var _7=require('./normalize');return _7.hasOwnProperty("normalize")?_7.normalize:_7.hasOwnProperty("default")?_7.default:_7}();
+var operate=function(){var _8=require('./operate');return _8.hasOwnProperty("operate")?_8.operate:_8.hasOwnProperty("default")?_8.default:_8}();
+var ifstmt=function(){var _9=require('./ifstmt');return _9.hasOwnProperty("ifstmt")?_9.ifstmt:_9.hasOwnProperty("default")?_9.default:_9}();
+var forstmt=function(){var _10=require('./forstmt');return _10.hasOwnProperty("forstmt")?_10.forstmt:_10.hasOwnProperty("default")?_10.default:_10}();
+var eventbus=function(){var _11=require('./eventbus');return _11.hasOwnProperty("eventbus")?_11.eventbus:_11.hasOwnProperty("default")?_11.default:_11}();
+var preVar=function(){var _12=require('./preVar');return _12.hasOwnProperty("preVar")?_12.preVar:_12.hasOwnProperty("default")?_12.default:_12}();
+var exprstmt=function(){var _13=require('./exprstmt');return _13.hasOwnProperty("exprstmt")?_13.exprstmt:_13.hasOwnProperty("default")?_13.default:_13}();
 
 var Token = homunculus.getClass('token', 'css');
 var Node = homunculus.getClass('node', 'css');
+
+var IGNORE = {};
+IGNORE[Node.IFSTMT]
+  = IGNORE[Node.FORSTMT]
+  = IGNORE[Node.BASENAME]
+  = IGNORE[Node.EXTNAME]
+  = IGNORE[Node.WIDTH]
+  = IGNORE[Node.HEIGHT]
+  = IGNORE[Node.ADDEXPR]
+  = IGNORE[Node.MTPLEXPR]
+  = IGNORE[Node.PRMREXPR]
+  = IGNORE[Node.FN]
+  = IGNORE[Node.VARSTMT]
+  = IGNORE[Node.UNBOX]
+  = IGNORE[Node.FNC]
+  = true;
 
 
   function Tree(ignores, index, varHash, globalVar, fnHash, globalFn, styleHash, styleTemp, selectorStack, map, focus, first, file) {
@@ -34,9 +50,6 @@ var Node = homunculus.getClass('node', 'css');
     this.file = file;
 
     this.res = '';
-    this.autoSplit = false;
-    this.inVar = false;
-    this.inOpt = false;
   }
   Tree.prototype.join = function(node) {
     var self = this;
@@ -46,62 +59,38 @@ var Node = homunculus.getClass('node', 'css');
         return;
       }
       eventbus.emit(node.nid());
-      //标识下一个string是否自动拆分
-      if(token.content() == '~' && token.type() != Token.HACK) {
-        self.autoSplit = true;
-        ignore(token, self.ignores, self.index);
-      }
-      else if(token.type() != Token.STRING) {
-        self.autoSplit = false;
-      }
-      if(!self.inOpt && (!token.ignore
-        || self.focus
-          && !self.inVar)) {
-        //~String拆分语法
-        if(self.autoSplit && token.type() == Token.STRING) {
-          var s = getVar(token, self.varHash, self.globalVar);
-          var c = s.charAt(0);
-          if(c != "'" && c != '"') {
-            c = '"';
-            s = c + s + c;
+      if(!token.ignore || self.focus) {
+        var str = getVar(token, self.varHash, self.globalVar);
+        //map映射url
+        if(token.import && self.map) {
+          var quote = /^['"']/.test(str) ? str.charAt(0) : '';
+          var val = quote ? str.slice(1, str.length - 1) : str;
+          //映射类型可能是回调
+          if(typeof self.map == 'function') {
+            str = self.map(val);
+            //如有引号，需处理转义
+            if(quote) {
+              str = quote + str + quote;
+            }
           }
-          s = s.replace(/,\s*/g, c + ',' + c);
-          self.res += s;
-          self.autoSplit = false;
+          else if(self.map.hasOwnProperty(token.val())){
+            str = self.map[val];
+            if(quote) {
+              str = quote + str + quote;
+            }
+          }
         }
-        else {
-          var str = getVar(token, self.varHash, self.globalVar);
-          //map映射url
-          if(token.import && self.map) {
-            var quote = /^['"']/.test(str) ? str.charAt(0) : '';
-            var val = quote ? str.slice(1, str.length - 1) : str;
-            //映射类型可能是回调
-            if(typeof self.map == 'function') {
-              str = self.map(val);
-              //如有引号，需处理转义
-              if(quote) {
-                str = quote + str + quote;
-              }
-            }
-            else if(self.map.hasOwnProperty(token.val())){
-              str = self.map[val];
-              if(quote) {
-                str = quote + str + quote;
-              }
-            }
+        //有@import url(xxx.css?xxx)的写法，需忽略
+        if(token.import && str.indexOf('.css?') == -1) {
+          //非.xxx结尾加上.css，非.css结尾替换掉.xxx为.css
+          if(!/\.\w+['"]?$/.test(str)) {
+            str = str.replace(/(['"]?)$/, '.css$1');
           }
-          //有@import url(xxx.css?xxx)的写法，需忽略
-          if(token.import && str.indexOf('.css?') == -1) {
-            //非.xxx结尾加上.css，非.css结尾替换掉.xxx为.css
-            if(!/\.\w+['"]?$/.test(str)) {
-              str = str.replace(/(['"]?)$/, '.css$1');
-            }
-            else if(!/\.css+['"]?$/.test(str)) {
-              str = str.replace(/\.\w+(['"]?)$/, '.css$1');
-            }
+          else if(!/\.css+['"]?$/.test(str)) {
+            str = str.replace(/\.\w+(['"]?)$/, '.css$1');
           }
-          self.res += str;
         }
+        self.res += str;
       }
       while(self.ignores[++self.index]) {
         var ig = self.ignores[self.index];
@@ -125,6 +114,10 @@ var Node = homunculus.getClass('node', 'css');
           break;
         case Node.FNC:
           self.res += getFn(node, self.ignores, self.index, self.fnHash, self.globalFn, self.varHash, self.globalVar);
+          var temp = ignore(node, self.ignores, self.index, true);
+          self.res += temp.res.replace(/[^\n]/g, '');
+          self.res += temp.append.replace(/\n/g, '');
+          self.index = temp.index;
           break;
         case Node.EXTEND:
           self.extend(node);
@@ -135,16 +128,12 @@ var Node = homunculus.getClass('node', 'css');
         case Node.ADDEXPR:
         case Node.MTPLEXPR:
         case Node.PRMREXPR:
-          var parent = node.parent();
-          if([Node.CALC, Node.ADDEXPR, Node.MTPLEXPR, Node.PRMREXPR].indexOf(parent.name()) == -1
-            && [Node.VARDECL, Node.CPARAMS].indexOf(parent.parent().name()) == -1
-            && !inFn(parent)
-            && parent.parent().name() != Node.EXPR) {
-            var opt = operate(node, self.varHash, self.globalVar, self.file);
-            self.res += opt.value + opt.unit;
-            ignore(node, self.ignores, self.index);
-            self.inOpt = true;
-          }
+          var opr = operate(node, self.varHash, self.globalVar, self.file);
+          self.res += opr.value + opr.unit;
+          var temp = ignore(node, self.ignores, self.index, true);
+          self.res += temp.res.replace(/[^\n]/g, '');
+          self.res += temp.append.replace(/\n/g, '');
+          self.index = temp.index;
           break;
         case Node.IFSTMT:
           var temp = ifstmt(
@@ -184,33 +173,75 @@ var Node = homunculus.getClass('node', 'css');
           self.res += temp.res;
           self.index = temp.index;
           break;
-        case Node.VARDECL:
-          //在if/for语句中会强制，外部var声明已在初期前置
-          if(self.focus) {
-            preVar(node, self.ignores, self.index, self.varHash, self.globalVar, self.file, self.focus);
-          }
-          //要忽略css3本身的var声明
-          else if(['$', '@'].indexOf(node.first().token().content().charAt(0)) > -1){
-            ignore(node, self.ignores, self.index);
-          }
-          break;
         case Node.VARSTMT:
-          self.inVar = true;
+          node.leaves().forEach(function(decl, i) {
+            //在if/for语句中会强制，外部var声明已在初期前置
+            if(self.focus) {
+              if(i % 2 == 0) {
+                if(['$', '@'].indexOf(node.first().first().token().content().charAt(0)) > -1) {
+                  preVar(decl, self.ignores, self.index, self.varHash, self.globalVar, self.file, self.focus);
+                  var temp = ignore(node, self.ignores, self.index, true);
+                  self.res += temp.res;
+                  self.index = temp.index;
+                }
+                else {
+                  var temp = join(decl, self.ignores, self.index);
+                  self.res += temp.str;
+                  self.index = temp.index;
+                }
+              }
+              //vardecl后的,或;
+              else {
+                var temp = ignore(node, self.ignores, self.index, true);
+                self.res += temp.res.replace(/[^\n]/g, '');
+                self.index = temp.index;
+              }
+            }
+            //要忽略css3本身的var声明
+            else {
+              if(['$', '@'].indexOf(node.first().first().token().content().charAt(0)) > -1) {
+                var temp = ignore(decl, self.ignores, self.index, true);
+                self.res += temp.res.replace(/[^\n]/g, '');
+                self.index = temp.index;
+              }
+              else {
+                var temp = join(decl, self.ignores, self.index);
+                self.res += temp.str;
+                self.index = temp.index;
+              }
+            }
+          });
           break;
         case Node.BASENAME:
         case Node.EXTNAME:
         case Node.WIDTH:
         case Node.HEIGHT:
-          if(!self.inVar && !self.inOpt) {
-            self.res += exprstmt(node, self.varHash, self.globalVar, self.file);
-          }
+          self.res += exprstmt(node, self.varHash, self.globalVar, self.file);
           var temp = ignore(node, self.ignores, self.index, true);
           self.res += temp.res.replace(/[^\n]/g, '');
           self.index = temp.index;
           break;
+        case Node.FN:
+          var temp = ignore(node, self.ignores, self.index, true);
+          self.res += temp.res.replace(/[^\n]/g, '');
+          self.index = temp.index;
+          break;
+        case Node.UNBOX:
+          var s = getVar(node.last().token(), self.varHash, self.globalVar);
+          var c = s.charAt(0);
+          if(c != "'" && c != '"') {
+            c = '"';
+            s = c + s + c;
+          }
+          s = s.replace(/,\s*/g, c + ',' + c);
+          self.res += s;
+          var temp = ignore(node, self.ignores, self.index, true);
+          self.res += temp.res;
+          self.index = temp.index;
+          break;
       }
       //递归子节点，if和for忽略
-      if([Node.IFSTMT, Node.FORSTMT, Node.BASENAME, Node.EXTNAME, Node.WIDTH, Node.HEIGHT].indexOf(node.name()) == -1) {
+      if(!IGNORE.hasOwnProperty(node.name())) {
         var leaves = node.leaves();
         leaves.forEach(function(leaf) {
           self.join(leaf);
@@ -220,20 +251,6 @@ var Node = homunculus.getClass('node', 'css');
       switch(node.name()) {
         case Node.STYLESET:
           self.styleset(false, node);
-          break;
-        case Node.VARSTMT:
-          self.inVar = false;
-          break;
-        case Node.ADDEXPR:
-        case Node.MTPLEXPR:
-        case Node.PRMREXPR:
-          var parent = node.parent();
-          if([Node.CALC, Node.ADDEXPR, Node.MTPLEXPR, Node.PRMREXPR].indexOf(parent.name()) == -1
-            && [Node.VARDECL, Node.CPARAMS].indexOf(parent.parent().name()) == -1
-            && !inFn(parent)
-            && parent.parent().name() != Node.EXPR) {
-            self.inOpt = false;
-          }
           break;
       }
     }
--- more/web/ignore.js
+++ more/web/ignore.js
@@ -1,10 +1,11 @@
-define(function(require, exports, module){var homunculus=require('homunculus');
+define(function(require, exports, module){var homunculus=function(){var _0=require('homunculus');return _0.hasOwnProperty("homunculus")?_0.homunculus:_0.hasOwnProperty("default")?_0.default:_0}();
 
 var Token = homunculus.getClass('token', 'css');
 var Node = homunculus.getClass('node', 'css');
 
 var res;
 var index;
+var append;
 
 function ignore(node, ignores, includeLine) {
   if(node instanceof Token) {
@@ -12,13 +13,15 @@ function ignore(node, ignores, includeLine) {
       return;
     }
     node.ignore = true;
+    append = '';
     while(ignores[++index]) {
       var ig = ignores[index];
       var s = ig.content();
-      if(ig.type() == Node.COMMENT && s.indexOf('//') == 0) {
+      if(ig.type() == Token.COMMENT && s.indexOf('//') == 0) {
         s = '/*' + s.slice(2) + '*/';
       }
       res += s;
+      append += s;
       if(includeLine || s != '\n') {
         ignores[index].ignore = true;
       }
@@ -37,6 +40,7 @@ function ignore(node, ignores, includeLine) {
 exports.default=function(node, ignores, i, includeLine) {
   res = '';
   index = i;
+  append = '';
   ignore(node, ignores, includeLine);
-  return { res:res, index:index };
+  return { res:res, index:index, append:append };
 };});
