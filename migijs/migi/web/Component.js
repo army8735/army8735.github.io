cc3d4f9f563947edd97a04bedb22280e9c0ed07e
army8735 <army8735@qq.com>
2015-05-02 15:08:06 +0800

    update

--- migijs/migi/web/Component.js
+++ migijs/migi/web/Component.js
@@ -1,50 +1,22 @@
 define(function(require, exports, module){var Event=function(){var _0=require('./Event');return _0.hasOwnProperty("Event")?_0.Event:_0.hasOwnProperty("default")?_0.default:_0}();
-var type=function(){var _1=require('./type');return _1.hasOwnProperty("type")?_1.type:_1.hasOwnProperty("default")?_1.default:_1}();
+var HtmlComponent=function(){var _1=require('./HtmlComponent');return _1.hasOwnProperty("HtmlComponent")?_1.HtmlComponent:_1.hasOwnProperty("default")?_1.default:_1}();
+var type=function(){var _2=require('./type');return _2.hasOwnProperty("type")?_2.type:_2.hasOwnProperty("default")?_2.default:_2}();
 
-!function(){var _2=Object.create(Event.prototype);_2.constructor=Component;Component.prototype=_2}();
-  function Component(name, props, chilren) {
-    if(props===void 0)props={};chilren=[].slice.call(arguments, 2);Event.call(this);
+!function(){var _3=Object.create(Event.prototype);_3.constructor=Component;Component.prototype=_3}();
+  function Component(name, props, children) {
+    if(props===void 0)props={};children=[].slice.call(arguments, 2);Event.call(this);
     this.name = name;
     this.props = props;
-    this.chilren = chilren;
-    this.on(Event.DOM, this.onDom);
-    this.on(Event.DATA, this.onData);
+    this.children = children;
   }
   //需要被子类覆盖
   Component.prototype.render = function() {
+    return '<' + this.name + '/>';
+  }
+  Component.prototype.toString = function() {
     var self = this;
-    var res = '<' + self.name;
-    Object.keys(self.props).forEach(function(k) {
-      res += ' ' + k + '="' + self.props[k] + '"'
-    });
-    res += '>';
-    self.chilren.forEach(function(child) {
-      res += self.renderChild(child);
-    });
-    res +='</' + self.name + '>';
-    return res;
-  }
-  Component.prototype.renderChild = function(child) {
-    var self = this;
-    if(child instanceof Component) {
-      return child.render();
-    }
-    else if(type.isArray(child)) {
-      var res = '';
-      child.forEach(function(item) {
-        res += self.renderChild(item);
-      });
-      return res;
-    }
-    else {
-      return child;
-    }
-  }
-  Component.prototype.onDom = function() {
-    //TODO
-  }
-  Component.prototype.onData = function(k, v) {
-    //TODO
+    var res = self.render();
+    return res.toString();
   }
 Object.keys(Event).forEach(function(k){Component[k]=Event[k]});
 
