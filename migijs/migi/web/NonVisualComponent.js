fa3cec990deccd4e5ac5c844391c0ceff3d2e17d
army8735 <army8735@qq.com>
2015-06-10 10:28:31 +0800

    update

--- migijs/migi/web/NonVisualComponent.js
+++ migijs/migi/web/NonVisualComponent.js
@@ -1,17 +1,25 @@
-define(function(require, exports, module){var Component=function(){var _0=require('./Component');return _0.hasOwnProperty("default")?_0["default"]:_0}();
+define(function(require, exports, module){var Event=function(){var _0=require('./Event');return _0.hasOwnProperty("default")?_0["default"]:_0}();
+var Component=function(){var _1=require('./Component');return _1.hasOwnProperty("default")?_1["default"]:_1}();
 
-!function(){var _1=Object.create(Component.prototype);_1.constructor=NonVisualComponent;NonVisualComponent.prototype=_1}();
+!function(){var _2=Object.create(Component.prototype);_2.constructor=NonVisualComponent;NonVisualComponent.prototype=_2}();
   function NonVisualComponent(data) {
     data=[].slice.call(arguments, 0);Component.apply(this,[].concat(Array.from(data)));
   }
 
   //非可视为空
   NonVisualComponent.prototype.toString = function() {
+    if(this.children.length) {
+      return Component.prototype.toString.call(this);
+    }
     return '';
   }
 
   //没有dom
-  NonVisualComponent.prototype.__onDom = function() {}
+  NonVisualComponent.prototype.__onDom = function() {
+    this.__dom = true;
+    //触发后就移除
+    this.off(Event.DOM, this.__onDom);
+  }
 Object.keys(Component).forEach(function(k){NonVisualComponent[k]=Component[k]});
 
 exports["default"]=NonVisualComponent;});
