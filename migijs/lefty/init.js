9fe52506c305e3a0e6343c89379317939a943076
army8735 <army8735@qq.com>
2015-06-11 14:10:09 +0800

    update

--- migijs/lefty/init.js
+++ migijs/lefty/init.js
@@ -1,10 +1,11 @@
 define(function(require, exports, module) {
-  var lefty = require('./index');
+  var lefty = require('./web/lefty')['default'];
   lefty.init = function() {
     var jsx = document.querySelectorAll('script');
     for(var i = 0, len = jsx.length; i < len; i++) {
       var node = jsx[i];
-      if(node.getAttribute('type') == 'text/jsx') {
+      if(node.getAttribute('type') == 'text/jsx' && !node.getAttribute('lefty')) {
+        node.setAttribute('lefty', 1);
         var code = node.textContent || node.innerText;
         if(!code) {
           continue;
