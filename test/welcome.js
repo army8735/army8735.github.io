fda663f404e20429283b3afcc7c9235d0599f819
army8735 <army8735@qq.com>
2017-07-13 15:16:08 +0800

    test

--- test/welcome.js
+++ test/welcome.js
@@ -55,8 +55,9 @@ setInterval(function() {
 }, 1000);
 
 var $yuyue = $('#yuyue');
-$yuyue.on('click', function() {
-  window.open('https://api.weibo.com/oauth2/authorize?client_id=4191980082&response_type=code&redirect_uri=http%3A%2F%2Farmy8735.github.io%2Ftest%2Fcb.html');
+$yuyue.on('click', function(e) {
+  e.preventDefault();
+  window.open('https://api.weibo.com/oauth2/authorize?client_id=4191980082&response_type=code&redirect_uri=http%3A%2F%2Farmy8735.github.io%2Ftest%2Ftest.html');
 });
 
 var now = new Date();
